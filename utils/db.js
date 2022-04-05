f9a6b13d5541f489bef903612f6ec65af5e0ae37
hoang <hoangphuc552001@gmail.com>
2022-04-05 17:16:00 +0700

    hosting

--- utils/db.js
+++ utils/db.js
@@ -2,11 +2,10 @@ import fn from "knex";
 const knex = fn({
     client: 'mysql2',
     connection: {
-        host : '127.0.0.1',
-        port : 3306,
-        user : 'root',
-        password : '',
-        database : 'realestate'
+        host : 'eu-cdbr-west-02.cleardb.net',
+        user : 'b578708cb52153',
+        password : '9f09c024',
+        database : 'heroku_eaea1e6277d01c8'
     },
     pool: { min: 0, max: 10 },
 });
