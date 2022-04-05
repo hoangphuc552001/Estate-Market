8d0c969a0c46820022a242fdf4f65e7830c39edd
hoang <hoangphuc552001@gmail.com>
2022-04-05 17:43:21 +0700

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -5,7 +5,7 @@ const knex = fn({
         host : 'eu-cdbr-west-02.cleardb.net',
         user : 'b578708cb52153',
         password : '9f09c024',
-        database : 'heroku_eaea1e6277d01c8'
+        database : 'heroku_eaea1e6277d01c8a'
     },
     pool: { min: 0, max: 10 },
 });
