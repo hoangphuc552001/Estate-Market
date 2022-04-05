4063a11dd37266e65ba62ee2c9504ec27d32a8e9
hoang <hoangphuc552001@gmail.com>
2022-04-05 17:22:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import activateSessionMiddleware from './middlewares/session.mdw.js'
 import asyncErrors from 'express-async-errors'
 //express declare
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 app.use('/public',express.static('public'))
 //morgan
 app.use(morgan('dev'));
